18d3ce5eda22282518d405e70c1c4e40c73d8341
Jillani Soft Tech <76724558+MGJillaniMughal@users.noreply.github.com>
2023-08-05 23:02:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ function PageTransitions() {
             element.classList.add('active');
         }
     })
-
     //Toggle theme
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click', () => {
